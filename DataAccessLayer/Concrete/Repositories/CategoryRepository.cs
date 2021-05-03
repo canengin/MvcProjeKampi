6240428ecfa9b87de759b6de97c8a04a8b6d005d
canengin <kcanengin@gmail.com>
2021-05-04 02:04:04 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>
+    {
+        
+    }
+    
+    
+}
+
+/*
+ * ToList
+ * Add
+ * Remove
+ * Find
+ */
