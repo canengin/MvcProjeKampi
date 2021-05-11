4b2f78dd4469eb5f1d913b4527b4cc03609cae77
canengin <kcanengin@gmail.com>
2021-05-12 02:01:44 +0300

    40.gün Görev 2/4 Repo Güncellendi

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,24 +10,40 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
 
-        public List<Category> GetAllBL()
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            return repo.List();
+            _categoryDal = categoryDal;
         }
-        public void CategoryAddBL(Category category)
+
+        public List<Category> GetList()
         {
-            if(category.CategoryName=="" || category.CategoryName.Length<=3 || category.CategoryDescription=="" || category.CategoryName.Length >== 51)
-            {
-                // hata mesajı 
-            }
-            else
-            {
-                repo.Insert(category);
-            }
+            return _categoryDal.List();
+        }
+        
+        public void CategoryAdd(Category category)
+        {
+            _categoryDal.Insert(category);
+            
+        }
+
+        public Category GetByID(int id)
+        {
+            return _categoryDal.Get(x=>x.CategoryID==id);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _categoryDal.Update(category);
         }
     }
 }
--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +14,43 @@ namespace MvcProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = categoryManager.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            //categoryManager.CategoryAddBL(category);
+            CategoryValidator categoryValidator = new CategoryValidator();
+
+            ValidationResult results = categoryValidator.Validate(category);
+            if (results.IsValid)
+            {
+                categoryManager.CategoryAdd(category);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var result in results.Errors)
+                {
+                    ModelState.AddModelError(result.PropertyName,result.ErrorMessage);
+                }
+            }
+
             return View();
         }
     }
